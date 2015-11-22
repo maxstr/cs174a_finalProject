4180300d048a0deacd4f884a43b2af5ead796260
Max Stritzinger <mstritzinger@live.com>
2015-11-21 17:13:19 -0800

    new dirs

---
+++ mysql/CreateTables.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS project;
+CREATE DATABASE project;
+
+USE project;
+
+CREATE TABLE Employees 
+    (id     INTEGER,
+     age    INTEGER,
+     SALARY TEXT,
+     PRIMARY KEY(id));
+
