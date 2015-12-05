79b42f5ce52c41efa06996e2a421c4618cec6d90
David Winkenwerder <rnkaufman@umail.ucsb.edu>
2015-12-04 17:37:33 -0800

    fixed

--- cryptoLib/WrappedCrypto.c
+++ cryptoLib/WrappedCrypto.c
@@ -3,34 +3,22 @@
 #include "paillier.h"
 #include <stdlib.h>
 #include <string.h>
+#include "WrappedCrypto.h"
 
-int key_pair_length = 128;
-typedef struct
-{
-  char* public;
-  char* private;
-}keys;
-
-
-keys generate_keys();
-void* encrypt_num(unsigned long int num,char* publkey);
-char* decrypt_num(void* encrypted, char* pubkey, char*privkey);
-void* homomorphic_add(void* val1, void* val2,char* publ);
+const int keySize = 2048;
 
 keys generate_keys()
 {
   keys retval;
   paillier_pubkey_t* publics;
   paillier_prvkey_t* privates;
-  paillier_keygen(key_pair_length,&publics,&privates,paillier_get_rand_devrandom);
+  paillier_keygen(keySize,&publics,&privates,paillier_get_rand_devrandom);
   retval.public = paillier_pubkey_to_hex(publics);
   retval.private = paillier_prvkey_to_hex(privates);
-  paillier_freepubkey(publics);
-  paillier_freeprvkey(privates);
   return retval;
 }
 
-void* encrypt_num(unsigned long int num,char *publkey)
+char* encrypt_num(unsigned long int num, char *publkey, int* size)
 {
   //make the pubkey from the string
   paillier_pubkey_t* publics = paillier_pubkey_from_hex(publkey);
@@ -49,15 +37,13 @@ void* encrypt_num(unsigned long int num,char *publkey)
   //return the result!
 
   void *result;
-  result = (char*)paillier_ciphertext_to_bytes(key_pair_length,encrypted);
+  *size = mpz_sizeinbase(encrypted->c, 2);
+  result = (char*) paillier_ciphertext_to_bytes(mpz_sizeinbase(encrypted->c, 2),encrypted);
   
-  paillier_freepubkey(publics);
-  paillier_freeplaintext(plain);
-  paillier_freeciphertext(encrypted);
   return result;
 }
 
-char* decrypt_num(void *to_unencrypt,char* pubkey,char*privkey)
+int decrypt_num(void *to_unencrypt,char* pubkey,char*privkey, int size)
 {
   //make a temp variable for encrypted
   //unsigned long int*uitemp = &to_unencrypt;
@@ -65,27 +51,20 @@ char* decrypt_num(void *to_unencrypt,char* pubkey,char*privkey)
 
   //make a ciphertext out of given encrypted text
   paillier_ciphertext_t* encrypted;
-  encrypted = paillier_ciphertext_from_bytes(to_unencrypt,key_pair_length);
+  encrypted = paillier_ciphertext_from_bytes(to_unencrypt, size);
 
   //make the public and private keys for doing the decryption
     paillier_pubkey_t* publics = paillier_pubkey_from_hex(pubkey);
     paillier_prvkey_t* privates = paillier_prvkey_from_hex(privkey,publics);
 
   //make a plaintext to store the result. 10 is just a stub, could be anything.
-  paillier_plaintext_t* plain;
-  plain = paillier_plaintext_from_ui(10);
 
-  paillier_dec(plain,publics,privates,encrypted);
-  char* result;
-  result = paillier_plaintext_to_str(plain);
-  paillier_freepubkey(publics);
-  paillier_freeprvkey(privates);
-  paillier_freeplaintext(plain);
-  paillier_freeciphertext(encrypted);
-  return result;
+  paillier_plaintext_t* plainText = paillier_dec(NULL,publics,privates,encrypted);
+
+  return (int) mpz_get_ui(plainText->m);
 }
 
-void* homomorphic_add(void *num1,void* num2, char* publ)
+void* homomorphic_add(void *num1,void* num2, char* publ,int size1, int size2, int* retsize)
 {
 
   //make a public key from the string
@@ -93,37 +72,43 @@ void* homomorphic_add(void *num1,void* num2, char* publ)
   
   //make ciphertexts out of given encrypted data
   paillier_ciphertext_t* encrypted1;
-  encrypted1 = paillier_ciphertext_from_bytes(num1,key_pair_length);
+  encrypted1 = paillier_ciphertext_from_bytes(num1,size1);
 
   paillier_ciphertext_t* encrypted2;
-  encrypted2 = paillier_ciphertext_from_bytes(num2,key_pair_length);
+  encrypted2 = paillier_ciphertext_from_bytes(num2,size2);
 
   
   //make a ciphertext for the result
-  paillier_ciphertext_t* result;
-  result = paillier_create_enc_zero();
+  paillier_ciphertext_t* ct;
+  ct = paillier_create_enc_zero();
 
-  
   //do the addition
-  paillier_mul(publics,result,encrypted1,encrypted2);
-
+  paillier_mul(publics,ct,encrypted1,encrypted2);
+  *retsize = mpz_sizeinbase(ct->c, 2);
+  void* result;
+ result = (char*) paillier_ciphertext_to_bytes(mpz_sizeinbase(ct->c, 2),ct); 
 
   //return result as iresult
-  return paillier_ciphertext_to_bytes(key_pair_length,result);
+ return result;
 }
 
 
 
 int main()
 {
-  keys bothkeys = generate_keys();
-  printf("%s,%s\n",bothkeys.public,bothkeys.private);
-  int done = 0;
-  char *result;
-      result = decrypt_num(encrypt_num(1234,bothkeys.public),bothkeys.public,bothkeys.private);
-      printf("%s\n",result);
-
+    keys bothkeys = generate_keys();
+    printf("Keys:\nPublic: %s\n\nPrivate: %s\n",bothkeys.public,bothkeys.private);
+    int size,size1,size2;
+    int result;
+    char *enc = encrypt_num(1234, bothkeys.public, &size);
+    char *enc2 = encrypt_num(1234,bothkeys.public,&size1); 
+    char *enc3 = homomorphic_add(enc,enc2,bothkeys.public,size,size1,&size2);
+    result = decrypt_num(enc3, bothkeys.public ,bothkeys.private, size2);
+    printf("Encrypted Text:\n%s\n",enc);
+    printf("Decrypted Number:\n%d\n",result);
   
-  return 0;
-}
+    return 0;
+
+    
+    }
 
